5d384da4a92e76db0a3c04147dacfb09a76ab479
Brennan Kersey <bksafari17@gmail.com>
2019-06-20 09:24:22 -0400

    Updated Main Menu with Collapse functionality

--- Source/Unbelievable/Data_handler_For_File_System.cpp
+++ Source/Unbelievable/Data_handler_For_File_System.cpp
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Data_handler_For_File_System.h"
+#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
+#include "Unbelievable_SaveGame.h"
+#include "Engine/Engine.h"
 
 // Sets default values
 AData_handler_For_File_System::AData_handler_For_File_System()
@@ -24,3 +27,33 @@ void AData_handler_For_File_System::Tick(float DeltaTime)
 
 }
 
+void AData_handler_For_File_System::LoadGame(int index)
+{
+	UUnbelievable_SaveGame* SaveGameInstance = Cast<UUnbelievable_SaveGame>(UGameplayStatics::CreateSaveGameObject(UUnbelievable_SaveGame::StaticClass()));
+	//SaveGameInstance = Cast<UUnbelievable_SaveGame>(UGameplayStatics::LoadGameFromSlot("MySlot", 0));
+	switch (index)
+	{
+	case 0:
+		SaveGameInstance = Cast<UUnbelievable_SaveGame>(UGameplayStatics::LoadGameFromSlot("MySlot", 0));
+		break;
+	case 1:
+		SaveGameInstance = Cast<UUnbelievable_SaveGame>(UGameplayStatics::LoadGameFromSlot("MySlot1", 0));
+		break;
+	case 2:
+		SaveGameInstance = Cast<UUnbelievable_SaveGame>(UGameplayStatics::LoadGameFromSlot("MySlot2", 0));
+		break;
+	case 3:
+		SaveGameInstance = Cast<UUnbelievable_SaveGame>(UGameplayStatics::LoadGameFromSlot("MySlot3", 0));
+		break;
+	case 4:
+		SaveGameInstance = Cast<UUnbelievable_SaveGame>(UGameplayStatics::LoadGameFromSlot("MySlot4", 0));
+		break;
+	}
+	//this->SetActorLocation(SaveGameInstance->PlayerLocation);
+	gameTimeForFile=SaveGameInstance->gameTimeTotal;
+	dateForFile = SaveGameInstance->dateOfLastPlay;
+	modeForFile = SaveGameInstance->modeOfPlay;
+	deathsForFile = SaveGameInstance->numberOfDeathes;
+	levelForFile = SaveGameInstance->levelPlayed;
+	//GEngine->AddOnScreenDebugMessage(-1, 15, FColor::Red, TEXT("Loaded Game For Main Menu"));
+}
